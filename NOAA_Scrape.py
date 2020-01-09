cd30d285ddf421ddf05b7b4db71269906cd352b1
Brett <brettjackson18@tamu.edu>
2020-01-08 18:14:56 -0600

    Added v2 API, which is better documented and more intuitive.

--- NOAA_Scrape.py
+++ NOAA_Scrape.py
@@ -6,20 +6,38 @@ Class for handling requests to NOAA API v1
 """
 import requests
 
-class NOAA:
+class NOAAv1:
     def __init__(self,token):
         self.data   = 'https://www.ncei.noaa.gov/access/services/data/v1?'
-        self.search = 'https://www.ncei.noaa.gov/access/services/search/v1?'
+        self.dataSearch = 'https://www.ncei.noaa.gov/access/services/search/v1/data?'
+        self.datasetSearch = 'https://www.ncei.noaa.gov/access/services/search/v1/dataset?'
         self.token = token
     def dataRequest(self,params):
         #Example: https://www.ncei.noaa.gov/access/services/data/v1?dataset=global-marine&dataTypes=WIND_DIR,WIND_SPEED&stations=AUCE&startDate=2016-01-01&endDate=2016-01-02&boundingBox=90,-180,-90,180
         params = {key: val for key, val in params.items() if val is not '' and val is not None}
         URL = self.data
         return requests.get(URL,headers={'token':self.token}, params=params)
-    def searchRequest(self,params):
-        #Example: https://www.ncei.noaa.gov/access/services/search/v1/data?dataset=daily-summaries&startDate=2010-01-01T00:00:00&endDate=2016-12-31T23:09:59&boundingBox=35.462327,-82.563951,35.412327,-82.513951&dataTypes=PRCP&stations=USW00003812&limit=10&offset=0
+    
+    def dataSearchRequest(self,params):
+        #Example: https://www.ncei.noaa.gov/access/services/search/v1/data?dataset=global-hourly&startDate=2016-01-01T00:00:00&endDate=2017-12-31T23:59:59&dataTypes=TMP&limit=10&offset=90
         params = {key: val for key, val in params.items() if val is not '' and val is not None}
-        URL = self.search
+        URL = self.dataSearch
+        return requests.get(URL,headers={'token':self.token}, params=params)
+    
+    def datasetSearchRequest(self,params):
+        params = {key: val for key, val in params.items() if val is not '' and val is not None}
+        URL = self.datasetSearch
+        return requests.get(URL,headers={'token':self.token}, params=params)
+    
+class NOAAv2:
+    def __init__(self,token):
+        self.base = 'https://www.ncdc.noaa.gov/cdo-web/api/v2/'
+        self.token = token
+    def request(self,params,receive,ID=None):
+        params = {key: val for key, val in params.items() if val is not '' and val is not None}
+        URL = self.base+receive
+        if ID:
+            URL += '/' + ID
         return requests.get(URL,headers={'token':self.token}, params=params)
         
 if __name__ == '__main__':
@@ -27,33 +45,102 @@ if __name__ == '__main__':
     with open('token.txt') as f:
         token = f.read()  
     print("Token: " + token)
-    query = NOAA(token)
+    query = NOAAv2(token)
     
-    dataParams = {'dataset':'global-marine',
-              'stations':'AUCE',
-              'startDate':'2016-01-01',
-              'endDate':'2016-01-02',
-              'dataTypes':'WIND_DIR,WIND_SPEED',
-              'boundingBox':'90,-180,-90,180',
-              'dataFormat':'',
-              'options':'',
-              'includeAttributes':'',
-              'includeStationName':'',
-              'includeStationLocation':'',
-              'units':''}
-    response = query.dataRequest(dataParams)
+    print("\n***\n*** DEFAULT 25 DATASETS\n***")
+    datasetParams = {'datatypeid':'',
+                  'locationid':'',
+                  'stationid':'',
+                  'startdate':'',
+                  'enddate':'',
+                  'sortfield':'',
+                  'sortorder':'',
+                  'limit':'',
+                  'offset':''}
+    response = query.request(datasetParams,'datasets')
     print(response.text)
     
-    searchParams = {'dataset':'daily-summaries',
-              'stations':'USW00003812',
-              'startDate':'2010-01-01T00:00:00',
-              'endDate':'2016-12-31T23:09:59',
-              'dataTypes':'PRCP',
-              'boundingBox':'35.462327,-82.563951,35.412327,-82.513951',
-              'limit':'10',
-              'offset':'0',
-              'keywords':'',
-              'text':'',
-              'available':''}
-    response2 = query.dataRequest(searchParams)
+    print("\n***\n*** DEFAULT 25 CATEGORIES\n***")
+    categoriesParams = {'datasetid':'',
+                  'locationid':'',
+                  'stationid':'',
+                  'startdate':'',
+                  'enddate':'',
+                  'sortfield':'',
+                  'sortorder':'',
+                  'limit':'',
+                  'offset':''}
+    response2 = query.request(categoriesParams,'datacategories')
     print(response2.text)
+    
+    print("\n***\n*** DEFAULT 25 TYPES\n***")
+    typesParams = {'datasetid':'',
+                  'locationid':'',
+                  'stationid':'',
+                  'datacategoryid':'',
+                  'startdate':'',
+                  'enddate':'',
+                  'sortfield':'',
+                  'sortorder':'',
+                  'limit':'',
+                  'offset':''}
+    response3 = query.request(typesParams,'datatypes')
+    print(response3.text)
+    
+    print("\n***\n*** DEFAULT 25 LOCATION CATEGORIES\n***")
+    loccatParams = {'datasetid':'',
+                  'startdate':'',
+                  'enddate':'',
+                  'sortfield':'',
+                  'sortorder':'',
+                  'limit':'',
+                  'offset':''}
+    response4 = query.request(loccatParams,'locationcategories')
+    print(response4.text)
+    
+    print("\n***\n*** DEFAULT 25 LOCATIONS\n***")
+    locParams = {'datasetid':'',
+                    'locationcategoryid':'',
+                    'datacategoryid':'',
+                    'startdate':'',
+                    'enddate':'',
+                    'sortfield':'',
+                    'sortorder':'',
+                    'limit':'',
+                    'offset':''}
+    response5 = query.request(locParams,'locations')
+    print(response5.text)
+    
+    print("\n***\n*** DEFAULT 25 STATIONS\n***")
+    stationParams = {'datasetid':'',
+                    'locationid':'',
+                    'datacategoryid':'',
+                    'datatypeid':'',
+                    'extent':'',
+                    'startdate':'',
+                    'enddate':'',
+                    'sortfield':'',
+                    'sortorder':'',
+                    'limit':'',
+                    'offset':''}
+    response6 = query.request(stationParams,'stations')
+    print(response6.text)
+    
+    print("\n***\n*** DEFAULT 25 DATAPOINT EXAMPLE\n***")
+    dataParams = {'datasetid':'GSOM',
+                    'datatypeid':'',
+                    'locationid':'',
+                    'stationid':'',
+                    'startdate':'2005-10-03',
+                    'enddate':'2012-09-10',
+                    'units':'',
+                    'sortfield':'',
+                    'sortorder':'',
+                    'limit':'',
+                    'offset':'',
+                    'includemetadata':''}
+    response7 = query.request(dataParams,'data')
+    print(response7.text)
+    
+    
+    
